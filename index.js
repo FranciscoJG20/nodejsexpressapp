ceb6b23bb29bb014e1997b85b6d347df83839b58
Francisco <fjgutierrez112@gmail.com>
2019-01-31 18:16:24 -0500

    commented out serving html files

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-//   res.end();
-// });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  res.end();
+});
 
 // used to deliver an html file to the server! An alternative is to use the sendFile() method
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
+// OR
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
 
 app.get("/profile/:name", (req, res) => {
   res.send(
