c73073a746f25e8805767753136374246c105cfa
Francisco <fjgutierrez112@gmail.com>
2019-01-31 13:38:53 -0500

    testing get requests

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
   res.end();
 });
 
+app.get("/profile/:name", (req, res) => {
+  res.send(
+    "you have requested to see a profile with the name of " + req.params.name
+  );
+});
+
 app.listen(4000, (req, res) => {
   console.log("a request was made!");
 });
